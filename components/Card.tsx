3891b92193cdf487426b88e5cedcd0a58dcf2378
Emilio Barboza <eabs291197@gmail.com>
2024-09-05 06:59:16 -0500

    feat: adding new components and variant for the card

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,33 +3,65 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from '../shared/constants/colors';
 import { CharacterInterface } from '../shared/interfaces/character';
 
-export const Card = (props: { item: CharacterInterface; onPress: () => void }) => {
-	const { item, onPress } = props;
+export const Card = (props: {
+	item: CharacterInterface;
+	onPress: () => void;
+	horizontal?: boolean;
+}) => {
+	const { item, onPress, horizontal } = props;
 
 	return (
 		<View style={styles.container}>
 			<Pressable onPress={onPress}>
-				<Image resizeMode="cover" source={{ uri: `${item.image}` }} style={styles.image} />
-				<View style={styles.innerContainer}>
+				{horizontal ? (
 					<View>
-						<Text style={styles.title}>{item.name}</Text>
 						<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-							<Ionicons
-								name="ellipse"
-								size={18}
-								color={item.status === 'Alive' ? Colors.green500 : Colors.red600}
-								style={{ marginEnd: 8 }}
-							/>
-							<Text style={styles.text}>
-								{item.status} - {item.species}
-							</Text>
+							<View style={styles.imageContainer}>
+								<Image
+									resizeMode="cover"
+									source={{ uri: `${item.image}` }}
+									style={styles.imageHorizontal}
+								/>
+							</View>
+							<View style={{ flex: 2, padding: 8, height: 120 }}>
+								<Text style={styles.title}>{item.name}</Text>
+								<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+									<Ionicons
+										name="ellipse"
+										size={18}
+										color={item.status === 'Alive' ? Colors.green500 : Colors.red600}
+										style={{ marginEnd: 8 }}
+									/>
+									<Text style={styles.text}>{item.status}</Text>
+								</View>
+							</View>
 						</View>
 					</View>
-					<View style={styles.infoContainer}>
-						<Text style={[styles.text, { color: Colors.gray100 }]}>Last known location:</Text>
-						<Text style={styles.text}>{item.location.name}</Text>
+				) : (
+					<View>
+						<Image resizeMode="cover" source={{ uri: `${item.image}` }} style={styles.image} />
+						<View style={styles.innerContainer}>
+							<View>
+								<Text style={styles.title}>{item.name}</Text>
+								<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+									<Ionicons
+										name="ellipse"
+										size={18}
+										color={item.status === 'Alive' ? Colors.green500 : Colors.red600}
+										style={{ marginEnd: 8 }}
+									/>
+									<Text style={styles.text}>
+										{item.status} - {item.species}
+									</Text>
+								</View>
+							</View>
+							<View style={styles.infoContainer}>
+								<Text style={[styles.text, { color: Colors.gray100 }]}>Last known location:</Text>
+								<Text style={styles.text}>{item.location.name}</Text>
+							</View>
+						</View>
 					</View>
-				</View>
+				)}
 			</Pressable>
 		</View>
 	);
@@ -66,6 +98,15 @@ const styles = StyleSheet.create({
 		borderColor: Colors.gray700,
 		borderBottomRightRadius: 14,
 		borderBottomLeftRadius: 14,
+	},
+	imageContainer: {
+		flex: 1,
+	},
+	imageHorizontal: {
+		height: 120,
+		borderBottomLeftRadius: 16,
+		borderTopLeftRadius: 16,
+		flex: 1,
 	},
 	infoContainer: {
 		marginTop: 20,
---
+++ components/SearchBar.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, TextInput, SafeAreaView, View, Pressable } from 'react-native';
+import { useState } from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { Colors } from '../shared/constants/colors';
+
+export const SearchBar = (props: { onSubmit: (value: string) => void }) => {
+	const { onSubmit } = props;
+
+	const [value, onChangeText] = useState<string>('');
+
+	return (
+		<SafeAreaView>
+			<View style={styles.container}>
+				<TextInput
+					style={styles.input}
+					inputMode="search"
+					placeholderTextColor={Colors.gray100}
+					placeholder="Search"
+					onChangeText={(text) => onChangeText(text)}
+					value={value}
+					onSubmitEditing={() => onSubmit(value)}
+					maxLength={100}
+				/>
+				{value && (
+					<Pressable
+						onPress={() => {
+							onSubmit('');
+							onChangeText('');
+						}}
+					>
+						<Ionicons name="close-circle" size={16} color={Colors.gray100} />
+					</Pressable>
+				)}
+			</View>
+		</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		marginHorizontal: 16,
+		marginVertical: 8,
+		borderWidth: 1,
+		borderRadius: 8,
+		padding: 8,
+		height: 40,
+		borderColor: Colors.gray700,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	input: {
+		flex: 1,
+		color: Colors.white,
+	},
+});
